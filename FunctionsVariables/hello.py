7c0267af6ab6d33a90feb2dd688d26d67e3efa28
Zacharee Mende <zachareeriley@gmail.com>
2023-08-07 05:05:13 -0700

    first commit

---
+++ FunctionsVariables/hello.py
@@ -0,0 +1,7 @@
+# Ask user for their name and remove whitespace from name and capitalize first letter
+name = input("Whats your name? ").strip().title()
+
+# Print hello and the user's name
+print(f"Hello, {name}!")
+
+# 1:00:00
